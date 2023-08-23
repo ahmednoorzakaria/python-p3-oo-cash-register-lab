88c2a16139ca76f45c638fe0a4f2a7c9914ee0aa
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-08-23 08:10:29 +0300

    cash

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.items = []
+        self.discount = discount
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.append((title, price, quantity))
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total = self.total * (1 - self.discount / 100)
+            print("After the discount, the total comes to ${}".format(self.total))
+
+    def get_items_list(self):
+        return [item[0] for item in self.items]
+
+    def void_last_transaction(self):
+        if len(self.items) > 0:
+            last_item = self.items.pop()
+            self.total -= last_item[1] * last_item[2]
+
